d376e0bff884653f84e9a2423fd7de04a4bfbaf0
Renaat Debleu <rdebleu@eWallah.net>
2026-04-19 17:05:11 +0000

    502

--- engine/google/version.php
+++ engine/google/version.php
@@ -31,4 +31,4 @@ $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v5.1.2';
 $plugin->version = 2026020100;
 $plugin->requires = 2025041400;
-$plugin->supported = [500, 501];
+$plugin->supported = [501, 502];
